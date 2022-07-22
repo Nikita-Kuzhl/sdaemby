0c714bba380a9ceb1e373da93504e64b1f24157c
Nikita-Kuzhl <kuzhleev@yandex.ru>
2022-07-22 17:40:41 +0300

    header

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Footer from './components/Footer';
+import Header from './components/Header';
+import './styles/index.scss'
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
