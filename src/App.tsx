daded003fedf34f5fc2bb6f024852ae999f3fc63
Никита Кужлев <nikitakuzlev@MacBook-Pro-Nikita.local>
2022-08-05 12:33:39 +0300

    registration

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,27 @@
 import { Route, Routes } from 'react-router-dom'
+import { useAppSelector } from './app/hooks'
 import Authorization from './pages/Authorization'
 import Contact from './pages/Contact/Contact'
 import Error from './pages/Error'
 import Main from './pages/Main'
 import News from './pages/News'
 import NewsList from './pages/NewsList'
+import Registration from './pages/Registration'
 import './styles/index.scss'
 const App = () => {
+  const { isAuth } = useAppSelector((state) => state.auth)
   return (
     <Routes>
       <Route path='/' element={<Main />} />
       <Route path='/contact' element={<Contact />} />
       <Route path='/news' element={<NewsList />} />
       <Route path='/news/:id' element={<News />} />
-      <Route path='/signin' element={<Authorization />} />
+      {!isAuth && (
+        <>
+          <Route path='/signin' element={<Authorization />} />
+          <Route path='/signup' element={<Registration />} />
+        </>
+      )}
       {/* Error */}
       <Route path='*' element={<Error />} />
     </Routes>
